a62de1808fcbaa24b62fcdfdf301464a90533dc0
rancetino1995 <tino.rance@gmail.com>
2026-09-09 16:38:05 -0500

    updates

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow, Menu } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    minWidth: 900,
+    minHeight: 560,
+    title: "Flap me Orbs",
+    backgroundColor: "#07080d",
+    autoHideMenuBar: true,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+      sandbox: true,
+    },
+  });
+
+  Menu.setApplicationMenu(null);
+  win.loadFile(path.join(__dirname, "game.html"));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
